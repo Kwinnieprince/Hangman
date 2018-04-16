6232b1b36639c8c5fbbf580589d8b5f9f7f1fc9a
Kwinnieprince <kwinten@delrue.com>
2018-04-16 12:06:49 +0200

    Made SpelerLauncher and Launcher

--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -1,5 +1,11 @@
 package Launcher;
 
+
 public class Launcher {
+
+    public static void main(String[] args){
+        SpelerLauncher speler = new SpelerLauncher();
+        speler.start();
+    }
 
 }
---
+++ src/Launcher/SpelerLauncher.java
@@ -0,0 +1,19 @@
+package Launcher;
+
+import domain.Speler;
+
+import javax.swing.*;
+
+
+public class SpelerLauncher {
+
+        public SpelerLauncher(){
+
+        }
+
+        public void start(){
+            String naam = JOptionPane.showInputDialog("Welkom! " + "\n" + "Hoe heet je?");
+            Speler speler = new Speler(naam);
+            JOptionPane.showMessageDialog(null,  speler.getNaam() + " heeft als score: " + speler.getScore());
+        }
+}
