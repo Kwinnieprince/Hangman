a93fa5fa62972c8ea7f05da352cc65da8e770a19
Kwinnieprince <kwinten@delrue.com>
2018-04-16 13:07:51 +0200

    Added comments

--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -4,8 +4,8 @@ package Launcher;
 public class Launcher {
 
     public static void main(String[] args){
-        SpelerLauncher speler = new SpelerLauncher();
-        speler.start();
+        SpelerLauncher speler = new SpelerLauncher(); //makes a new Speler
+        speler.start(); //calls the function Start from the speler
     }
 
 }
--- src/Launcher/SpelerLauncher.java
+++ src/Launcher/SpelerLauncher.java
@@ -13,9 +13,11 @@ public class SpelerLauncher {
         }
 
         public void start(){
-
-                String naam = JOptionPane.showInputDialog("Welkom! " + "\n" + "Hoe heet je?");
-                Speler speler = new Speler(naam);
+                while(naam == null || naam.trim().isEmpty()) {
+                    // gives the optionPane when the input is NULL or empty
+                    naam = JOptionPane.showInputDialog("Welkom! " + "\n" + "Hoe heet je?");
+                } //TODO try-catch block when there is expantion
+                Speler speler = new Speler(naam); // A new speler is created with the name
                 JOptionPane.showMessageDialog(null, speler.getNaam() + " heeft als score: " + speler.getScore(), speler.getNaam(), 1);
 
 
