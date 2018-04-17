d64652220e5487d8a1db03738f45a212b92fd422
Kwinnieprince <kwinten@delrue.com>
2018-04-17 10:37:21 +0200

    finish of story 4 frontend

--- src/Launcher/PictionaryUi.java
+++ src/Launcher/PictionaryUi.java
@@ -7,18 +7,34 @@ import javax.swing.*;
 
 public class PictionaryUi {
 	private Speler speler;
+	int xInt;
+	int yInt;
+	Punt punt;
 
         public PictionaryUi(Speler speler){
 
         }
 
         public void showMenu() {
-        	int xInt = Integer.parseInt(JOptionPane.showInputDialog("X coordinaat van het punt: "));
-            int yInt = Integer.parseInt(JOptionPane.showInputDialog("Y coordinaat van het punt: "));
+        	xInt = Integer.parseInt(JOptionPane.showInputDialog("X coordinaat van het punt: "));
+            yInt = Integer.parseInt(JOptionPane.showInputDialog("Y coordinaat van het punt: "));
+            punt = new Punt(xInt, yInt);
+
+        }
+
+        public void ifCirckel(){
             int radiusInt = Integer.parseInt(JOptionPane.showInputDialog("Radius van de circkel: "));
-            Punt punt = new Punt(xInt, yInt);
             Cirkel cirkel = new Cirkel(new Punt(xInt, yInt), radiusInt);
             JOptionPane.showMessageDialog(null, "U heeft een correcte circkel gemaakt: " + cirkel.toString());
+        }
+
+        public void  ifRechthoek(){
+
+            int breedteInt = Integer.parseInt(JOptionPane.showInputDialog("Wat is de breedte van de rechthoek?"));
+            int hoogteInt = Integer.parseInt(JOptionPane.showInputDialog("Wat is de hoogte van de rechthoek?"));
+            Rechthoek rechthoek = new Rechthoek(punt, breedteInt, hoogteInt);
+            JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek gemaakt: " + rechthoek.toString());
+
         }
 
 
