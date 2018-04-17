bf36197fa50ec402e308f319525d9a49d7024494
michael <michael.creelle@telenet.be>
2018-04-17 14:55:18 +0200

    fixed some errors

--- src/domain/Vorm.java
+++ src/domain/Vorm.java
@@ -1,8 +1,6 @@
 package domain;
 
 public abstract class Vorm implements Drawable {
-
-	public abstract Omhullende getOmhullende();
 	
 	public boolean equals(Object o) {
 		if (o == null) return false;
