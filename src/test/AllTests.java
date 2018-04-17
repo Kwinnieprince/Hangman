c6372ca450ed589ebafad9a6d95e6dfc62358e8b
michael <michael.creelle@telenet.be>
2018-04-17 15:05:54 +0200

    alle tests bijgevult in alltests

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,7 +5,17 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class})
+@SuiteClasses({ PuntTest.class, 
+				SpelerTest.class, 
+				CirkelTest.class, 
+				DriehoekTest.class, 
+				HintLetterTest.class,
+				HintWoordTest.class,
+				LijnStukTest.class,
+				OmhullendeTest.class,
+				TekeningTest.class,
+				WoordenLijstTest.class
+				})
 public class AllTests {
 
 }
